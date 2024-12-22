32f09f9952537289c834eeaf5bdd7b1653040ceb
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-22 14:58:29 +0300

    create a fundation for rentalController

---
+++ controller/rentalController.js
@@ -0,0 +1,42 @@
+const testRental = [
+    {
+        "id": 10,
+        "move_Name": "movie_test_1",
+        "user_Name": "user_test_1",
+        "rental_Date": "2022-01-21",
+        "return_Date": "2022-01-24"
+    },
+    {
+        "id": 11,
+        "move_Name": "movie test 2",
+        "user_Name":  "user test 2",
+        "rental_Date": "2022-01-22",
+        "return_Date": "2022-01-25"
+    }
+]
+
+export function getRentals   (req,res){
+    res.json({
+        "message": "rental feauter under constraction "
+    })
+}
+export function getRentalID (req,res){
+    res.json({
+        "message": "rental feauter under constraction "
+    })
+}
+export function createRental(req,res){
+    res.json({
+        "message": "rental feauter under constraction "
+    })
+}
+export function updateRental(req,res){
+    res.json({
+        "message": "rental feauter under constraction "
+    })
+}
+export function deleteRental(req,res){
+    res.json({
+        "message": "rental feauter under constraction "
+    })
+}
