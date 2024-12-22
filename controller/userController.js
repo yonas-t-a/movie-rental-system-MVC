e5f4e3e904f802b46ea11c359c067fea650b355a
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-22 15:31:02 +0300

    user controller basic setup

---
+++ controller/userController.js
@@ -0,0 +1,38 @@
+const testUser = [
+    {
+        "id": 31,
+        "name": "user_test_1",
+        "email": "a@b.c"
+    },
+    {
+        "id": 32,
+        "name": "user_test_2",
+        "email": "x@y.z"
+    }
+]
+
+export function getUsers    (req, res){
+    res.json({
+        "message": "user feauter under constraction"
+    })
+}
+export function getUserID   (req, res){
+    res.json({
+        "message": "user feauter under constraction"
+    })
+}
+export function createUser (req, res){
+    res.json({
+        "message": "user feauter under constraction"
+    })
+}
+export function updateUser (req, res){
+    res.json({
+        "message": "user feauter under constraction"
+    })
+}
+export function deleteUser (req, res){
+    res.json({
+        "message": "user feauter under constraction"
+    })
+}
