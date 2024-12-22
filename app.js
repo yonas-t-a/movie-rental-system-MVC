958caafbe9bbe786a7d28377c601848741d4f673
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-22 16:00:24 +0300

    config route and handle static filesand middleware

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import rental from './routers/api/rentalApi.js'
+import movie from './routers/api/movieApi.js'
+import user from './routers/api/userApi.js'
+import root from './routers/root.js'
+
+import {fileURLToPath} from 'url'
+import {dirname, join} from 'path'
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
+
+const staticFileLocation = join(__dirname, 'view')
+
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static(staticFileLocation))
+
+app.use("/", root)
+app.use('/rental', rental)
+app.use('/movie', movie)
+app.use('/user', user)
+
+
+app.listen(PORT, ()=>{
+    console.log(`The app is listenting at port ${PORT}`)
+})
