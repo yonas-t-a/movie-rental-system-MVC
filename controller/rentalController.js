cb5e07f68650932352a1fdd1656080ffd5e2052a
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-26 15:00:08 +0300

    rental/controller

--- controller/rentalController.js
+++ controller/rentalController.js
@@ -1,26 +1,52 @@
+import RentalModel from "../model/rentalModel.js"
 
-export function getRentals   (req,res){
-    res.json({
-        "message": "rental feauter under constraction "
-    })
+export async function getRentals   (req,res){
+    try {
+        const result = await RentalModel.getAllRental();
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send('Error in gatting all rentals')
+    }
 }
-export function getRentalID (req,res){
-    res.json({
-        "message": "rental feauter under constraction "
-    })
+export async function getRentalID (req,res){
+    const id = req.params.id
+    try {
+        const result  =  RentalModel.getRentalById(id);
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send('Error in gatting  rental by ID')
+    }
 }
-export function createRental(req,res){
-    res.json({
-        "message": "rental feauter under constraction "
-    })
+export async function createRental(req,res){
+    const {user_id, movie_id, rental_date, return_date } = req.body
+    try {
+        await RentalModel.insertRental(user_id, movie_id, rental_date, return_date)
+    } catch (error) {
+        res.status(500).send('Error in gatting inserting rental')
+    }
 }
-export function updateRental(req,res){
-    res.json({
-        "message": "rental feauter under constraction "
-    })
+export async function updateRental(req,res){
+    const id = req.params.id;
+    const  {user_id, movie_id, rental_date, return_date } = req.body
+
+    const updatedUserId = user_id || RentalModel.rentalUser(id);
+    const updatedMovieId = movie_id || RentalModel.rentalMovie(id);
+    const updatedRentalDate = rental_date || RentalModel.rentalDate(id);
+    const updateReturnDate = return_date || RentalModel.rentalReturnDate(id);
+
+    try {
+        await RentalModel.updateRental(id, updatedUserId, updatedMovieId, updatedRentalDate, updateReturnDate);
+        res.status(200).send('The rental is sucessfully updated')
+    } catch (error) {
+        res.status(500).send('Error in updating rental')
+    }
 }
-export function deleteRental(req,res){
-    res.json({
-        "message": "rental feauter under constraction "
-    })
+export async function deleteRental(req,res){
+    const id = req.params.id;
+    try {
+        await RentalModel.DeleteRental(id);
+        res.status(200).send("The rental is sucessfully deleted")
+    } catch (error) {
+        res.status(500).send('Error in delating rentals')
+    }
 }
