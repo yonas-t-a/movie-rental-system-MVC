116fcc639ee65c7f5c8beddd2c0306e779b61aac
yonas-t-a <yonas.tessema@aait.edu.et>
2024-12-26 10:10:43 +0300

    import UserModle, and impliment getUsers, getUsersID, createUser, UpdateUser, deleteUsee functionality with error handing for each one of them

--- controller/userController.js
+++ controller/userController.js
@@ -1,27 +1,50 @@
+import UserModel from "../model/userModel.js"
 
+export async function getUsers (req, res){
+    try {
+        const result= await UserModel.getAllUser()
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send("Error in fetching the user")
+    }
+}
+export async function getUserID   (req, res){
+    const {id} = req.params.id
+    try {
+        const result = await UserModel.getUserbyId(id);
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send('Error in fetching user with id')
+    }
+}
+export async function createUser (req, res){
+   const {name, email} = req.body;
+   try {
+    await UserModel.insertUser(name, email);    
+    res.status(201).send('User sucessfully added');
+   } catch (error) {
+        res.status(500).send('Error in creating user')
+   }
+}
+export async function updateUser (req, res){
+    const {id}=req.params.id;
+    try {
+        const {name, email} = req.body;
 
-export function getUsers    (req, res){
-    res.json({
-        "message": "user feauter under constraction"
-    })
+        const updatedName = name || UserModel.userName;
+        const updatedEmail = email || UserModel.userEmail;
+
+        await UserModel.updateUser(updatedName, updatedEmail)
+        res.status(200).send('The user is sucessfully updated')
+    } catch (error) {
+        res.status(500).send('Error while updating the user')
+    }
 }
-export function getUserID   (req, res){
-    res.json({
-        "message": "user feauter under constraction"
-    })
-}
-export function createUser (req, res){
-    res.json({
-        "message": "user feauter under constraction"
-    })
-}
-export function updateUser (req, res){
-    res.json({
-        "message": "user feauter under constraction"
-    })
-}
-export function deleteUser (req, res){
-    res.json({
-        "message": "user feauter under constraction"
-    })
+export async function deleteUser (req, res){
+    const {id} = req.params.id;
+    try {
+        
+    } catch (error) {
+        res.status(500).send("Error While delating the user with the given Id")
+    }
 }
